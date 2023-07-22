5ec03a64c5c01ffdb0904068317ba2309ccca31b
pseudovicky <sudovicky@gmail.com>
2023-07-22 23:33:33 +0530

    [ printing to the console ]

--- Day 001/Printing_start.py
+++ Day 001/Printing_start.py
@@ -1,7 +1,7 @@
 print("Hello,World!")
 
 '''
-print() is a simple function which is prints all the message which is available between dauble quots. 
+print() is a simple function which is prints all the message which is available between dauble quotes. 
 where  double quotes is just a reminder for string ends.
 string is a data type in python programming language. 
 string simply means a collection of characters.
---
+++ Day 001/string_manipulation.py
@@ -0,0 +1,8 @@
+print("Hello world!\nHello World!")
+
+# so \n is generaly use for break or separate the line.
+
+print('''\nHello world!\nHello 
+World!\n''')
+
+# multiline string by using ''' '''
