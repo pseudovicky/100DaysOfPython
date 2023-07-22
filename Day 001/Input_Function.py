61635f6c06b38459c4187da907c79f18b4dce852
pseudovicky <sudovicky@gmail.com>
2023-07-23 00:50:11 +0530

    The Python Input Function

---
+++ Day 001/Input_Function.py
@@ -0,0 +1,12 @@
+'''
+input() function
+input("A prompt for the user")
+'''
+print("What is your name?")
+input("What is your name?") # input value store itself in current input function.
+
+print("Hello" + " " + input("What is your name?") + "!" ) 
+# input() will get user input in consol
+# Then print() will print the word "Hello" and the user input.
+
+# Use Thonny IDE or Python Tutor for better understanding and find running sequences of Code.
