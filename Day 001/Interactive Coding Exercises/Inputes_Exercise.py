bf261b451e14ce209b7eeabc69edff3f12758519
pseudovicky <sudovicky@gmail.com>
2023-07-23 02:23:04 +0530

    inputs Exercise len() function

--- Day 001/Interactive Coding Exercises/Inputes_Exercise.py
+++ Day 001/Interactive Coding Exercises/Inputes_Exercise.py
@@ -2,5 +2,6 @@
 Q.} Write a program that prints the nummber of characters in a user's name.
 Hint :- you might need to Google for a function that calculates the length of a string.
 '''
-print( len( input("What is your name? ") ) )
+print( len( input("What is your name? ") ) ) 
+# count character by using len() function
 
