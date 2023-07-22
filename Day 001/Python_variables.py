7ff1196200789332cf19c036c7bd4c1b6a3121cf
pseudovicky <sudovicky@gmail.com>
2023-07-23 02:38:31 +0530

    python variable

---
+++ Day 001/Python_variables.py
@@ -0,0 +1,13 @@
+
+name = input("What is your name?")
+print(name)
+
+username = "jack"
+print(username)
+
+username = "Angela"
+print(username)
+
+Name = input("What is your name?")
+length = len(name)
+print(length)
