e0fe28c352010c62df82b7c62903f37c9fa8cddf
pseudovicky <sudovicky@gmail.com>
2023-07-23 00:19:44 +0530

    Debugging Practice

---
+++ Day 001/Interactive Coding Exercises/Debugging_practice..py
@@ -0,0 +1,4 @@
+print("Day 1 - String Manipulation") # fixed missing Double  Quotes .
+print('String Conncatination is done with the "+" sign.') # put all the strings in single quotes or in othor way we can use \ before quotes which is between the strings that will also works.
+print('e.g. print("Hello" + "World")') # Fixed Indentation Error
+print("New lines can be created wth a backslash ans n.") # removed extra small braces.
