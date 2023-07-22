886f55db1f17610c0197230dc69a071bb9f5c629
pseudovicky <sudovicky@gmail.com>
2023-07-22 22:27:13 +0530

    Printing Start program

---
+++ Day 001/Printing_start.py
@@ -0,0 +1,8 @@
+print("Hello,World!")
+
+'''
+print() is a simple function which is prints all the message which is available between dauble quots. 
+where  double quotes is just a reminder for string ends.
+string is a data type in python programming language. 
+string simply means a collection of characters.
+'''
