7d829222a701ce4d5a663deb17b4831e9b514965
pseudovicky <sudovicky@gmail.com>
2023-07-22 23:54:47 +0530

    String manipulation and Code Intelligence

--- Day 001/string_manipulation.py
+++ Day 001/string_manipulation.py
@@ -5,4 +5,17 @@ print("Hello world!\nHello World!")
 print('''\nHello world!\nHello 
 World!\n''')
 
-# multiline string by using ''' '''
+# multiline string by using ( ''' some string ''' )
+
+
+# concatenate strings - concatenation using  '+' (plus)  Arithematic operator.
+
+print("Hello" + " " + "Vicky")
+"Hello Vicky"
+
+# Indentation - No use extra spaces Otherwise it gives an error called IndentationError: unexpected indent .
+# so allways start your code in the beginning of the line.
+print('hello World!') # like this: 
+
+# use code Intelligence for find and solve the errors in your code. 
+# In term of programming errors solving in code is called debugging.
