023d395207aaefb1c145587b6c9ffae3e4c6655b
pseudovicky <sudovicky@gmail.com>
2023-07-23 14:48:04 +0530

    Number manupulation and f-String

---
+++ Day 002/Number_manipulation.py
@@ -0,0 +1,24 @@
+print(10/3) # true division
+
+print(10//3) # Floor division
+
+print( 10%3) # modulus 
+
+
+# Assignment Operators 
+
+score = 0
+# user score point
+score += 1
+print("score = " ,score)
+score += 5
+print("score = " ,score)
+print("Your Score is " + str(score))
+# F-String 
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+
+
