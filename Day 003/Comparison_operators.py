22b887d56f561f5bd4a742c7f852b333b333bbb4
pseudovicky <sudovicky@gmail.com>
2023-07-24 03:06:01 +0530

    Comparison Operators

---
+++ Day 003/Comparison_operators.py
@@ -0,0 +1,12 @@
+# conditional operators
+
+'''
+Greater than   >
+less than      <
+greater than or equal to >=
+less than or equal to    <=
+equal to   ==
+not equal to !=
+
+'''
+# it is also called Assignment Operator .
