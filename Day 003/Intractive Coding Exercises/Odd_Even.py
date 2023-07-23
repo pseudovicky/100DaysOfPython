ec8d892efed1bca88fb8f757971e441e9dd1d6a3
pseudovicky <sudovicky@gmail.com>
2023-07-24 03:16:57 +0530

    Odd Even check program and Modulo Operator

---
+++ Day 003/Intractive Coding Exercises/Odd_Even.py
@@ -0,0 +1,15 @@
+# Write a program that works out whether if a given numbers is an odd or even numbers.
+# Even number can be divided by 2 with no remainder.
+
+# Modulo Operation
+
+#  Don't change the code below
+number = int(input("Which number do you want to check odd or even Enter your number : "))
+# Don't change the code below
+
+#  Write your code below rhis line
+
+if number % 2 == 0:
+    print("This is an Even number.")
+else:
+    print("This is an Odd number.")
