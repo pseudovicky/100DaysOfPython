79027c2b8efbff8295f7c0903ec6a22095abc6ef
pseudovicky <sudovicky@gmail.com>
2023-07-24 03:40:30 +0530

    nested if else

---
+++ Day 003/Nested_if_else.py
@@ -0,0 +1,14 @@
+#  Nested Conditional Statement
+
+print("Welcome to the rollercoaster ride !")
+height = int(input("Enter your height in cm? "))
+
+if height >= 120:
+    print("Yes you can ride")
+    age = int(input("Now, tell me your age in Year !!"))
+    if age <= 18:
+        print("You have to pay $7")
+    else:
+        print("You have to pay $12")
+else:
+    print("Sorry, You Can't Ride. you have to grow taller before you can ride.")
