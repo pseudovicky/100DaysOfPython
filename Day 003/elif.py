ce4ace84044bf53f1ae28a4b987f07a23de1e7b3
pseudovicky <sudovicky@gmail.com>
2023-07-24 03:46:03 +0530

    elif conditional operator

--- Day 003/elif.py
+++ Day 003/elif.py
@@ -6,11 +6,12 @@ height = int(input("Enter your height in cm? "))
 if height >= 120:
     print("Yes you can ride")
     age = int(input("Now, tell me your age in Year !!"))
-    if age < 12:
+    if age <= 12:
         print("You have to pay $5")
-    elif age > 12 & age < 18 :
+    elif age > 12 and age <= 18 :
         print("You have to pay $7")
     else:
         print("You have to pay $12")
 else:
     print("Sorry, You Can't Ride. you have to grow taller before you can ride.")
+
