1a0c49ed6b0dc2dc33b845bb52f0b10f1c817078
pseudovicky <sudovicky@gmail.com>
2023-07-24 12:44:57 +0530

    Project Treasure Island

--- Day 003/Day 03 Project/Treasure_Island.py
+++ Day 003/Day 03 Project/Treasure_Island.py
@@ -1,16 +1,39 @@
 # Project Treasure Island Day 03
 
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[TomekK]
+******************************************************************************* 
+''')
+
 print("Welcome to the Treasure Island\nYour mision is to find the treasure.")
 direction = input(''' You're at a cross road. Where do you want to go? Type "left" or "right": ''')
-if direction == "left" :
+if direction.lower() == "left" :
     option = input('''You came to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. ''')
-    if option == "wait":
+    if option.lower() == "wait":
         door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you you choose? ")
-        if door == "yellow":
+        if door.lower() == "yellow":
             print("You Win!")
         else:
-            print("Game Over.")
+            print("It's a room full of fire. Game Over.")
     else:
-        print("Game Over.")
+        print("You got attacked by an angry trout. Game Over.")
 else:
-    print("Game Over.")
+    print("You fell into a hole. Game Over.")
