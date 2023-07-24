ab5063a8895eeca79c2e45b9b9ed06e76a49292f
pseudovicky <sudovicky@gmail.com>
2023-07-24 10:43:37 +0530

    roallercoaster ticket programm 2.0

---
+++ Day 003/Multiple_if_succession.py
@@ -0,0 +1,28 @@
+#  RollerCoaster ride Ticket Program.
+
+print("Welcome to the roallerCoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+    print("You acn ride the rollerCoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("child tickets are $5.")
+        bill += 5
+    elif age <= 18:
+        print("Youth tickets are $7.")
+        bill += 7
+    else:
+        print("Adult tickets are $12.")
+        bill += 12
+    
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y" or "y" :
+        # Add $3 to their bill
+        bill += 3
+    
+    print(f"Your final Bill is {bill}.")
+
+else:
+    print("Sorry, you have to grow taller before you can ride.")
