1f942a618f28cf537091a2cda00d654b43d53ae0
pseudovicky <sudovicky@gmail.com>
2023-07-25 11:17:27 +0530

    Python List Data Structure

---
+++ Day 004/List_DataStructure.py
@@ -0,0 +1,14 @@
+
+states_of_india = ["Bihar", "Maharastra","Delhi","Kerala","Up","Bangal","Mp","Taminadu","Goa","Jharkhan","Utrakhand","Manipur"]
+
+print(states_of_india[0])
+print(states_of_india[1])
+print(states_of_india[2])
+print(states_of_india[-3])
+
+print(states_of_india[1])
+states_of_india[1] = "Nagaland"
+print(states_of_india[1])
+
+states_of_india.append("Panjab")
+print(states_of_india)
