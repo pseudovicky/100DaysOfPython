2fb54d115eccd4de524699de81fc22bea0504096
pseudovicky <sudovicky@gmail.com>
2023-07-27 02:48:16 +0530

    functions

---
+++ Day 006/Functions.py
@@ -0,0 +1,8 @@
+# difining functions usng def keyword
+def my_function():
+    print("Hello,")
+    print("This is a function.")
+    print("Bye!")
+
+#  Calling Functions just the specify the name and prenthesis .
+my_function()
