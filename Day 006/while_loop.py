16fece6763e37c0cd06eeea2ab772379d59e8efd
pseudovicky <sudovicky@gmail.com>
2023-07-27 12:27:28 +0530

    The hurdles loop challenge using for loop

---
+++ Day 006/while_loop.py
@@ -0,0 +1,24 @@
+# The Hurdles Loop Challenge
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def repetitive_block():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+number_of_hurdles = 6
+while (number_of_hurdles > 0):
+    repetitive_block()
+    number_of_hurdles -= 1
+
+    
+    
