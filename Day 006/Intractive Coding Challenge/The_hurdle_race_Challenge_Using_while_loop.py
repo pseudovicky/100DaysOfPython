13e68c9e5c993eb6ff3b24e563b571f51bb1dd2f
pseudovicky <sudovicky@gmail.com>
2023-07-27 12:41:54 +0530

    The hurdles race challenge using while loop

---
+++ Day 006/Intractive Coding Challenge/The_hurdle_race_Challenge_Using_while_loop.py
@@ -0,0 +1,25 @@
+# The Hurdles Loop Challenge
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def repetitive_block():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        repetitive_block()
+    else:
+        move()
+
+
+    
+    
