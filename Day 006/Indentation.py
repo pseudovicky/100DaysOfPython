6945d7c0b44585651989967d2139a6332001400d
pseudovicky <sudovicky@gmail.com>
2023-07-27 12:09:33 +0530

    Indentation

---
+++ Day 006/Indentation.py
@@ -0,0 +1,16 @@
+def my_function():
+    print("hello") # indented by 4 space 
+print("Bye") # independente block 
+
+def my_functions():
+    sky = "clear"
+    if sky == "clear":
+        print("blue")
+    elif sky == "Cloudy":
+        print("Grey")
+    print("Hello,")
+print("World")
+
+my_functions()
+
+    
