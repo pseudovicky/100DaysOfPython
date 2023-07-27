cad0301751dda12befbf5513fb9b120c2a06e0d6
pseudovicky <sudovicky@gmail.com>
2023-07-27 11:02:01 +0530

    Reeborgs World

---
+++ Day 006/Reeborgs_World.py
@@ -0,0 +1,33 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=%2Fworlds%2Fmenus%2Fselect_collection_en.json&name=Alone&url=%2Fworlds%2Ftutorial_en%2Falone.json
+'''
+move()
+move()
+move()
+turn_left()
+move()
+move()
+move()
+
+
+move()
+move()
+turn_left()
+turn_left()
+move()
+move()
+turn_left()
+turn_left()
+
+create a turn around function :
+def turn_around():
+    turn_left()
+    turn_left()
+    
+move()
+move()
+turn_around()
+move()
+move()
+turn_around()
+
+'''
---
+++ Day 006/turn_right_challenge.py
@@ -0,0 +1,19 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=%2Fworlds%2Fmenus%2Fselect_collection_en.json&name=Alone&url=%2Fworlds%2Ftutorial_en%2Falone.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+turn_left()
+move()
+move()
+turn_right()
+move()
+move()
+turn_right()
+move()
+move()
+turn_right()
+move()
+move()
+
