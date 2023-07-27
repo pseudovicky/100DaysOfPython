c55bf29b578233dda3941954135c3d2fabb7e4cc
pseudovicky <sudovicky@gmail.com>
2023-07-27 19:03:24 +0530

    Escaping the maze Day 6 project

---
+++ Day 006/Day 06 Project/Escaping_the_Maze.py
@@ -0,0 +1,17 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# Day 6 project Escaping the maze
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+        
