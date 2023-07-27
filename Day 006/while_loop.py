87396311ee8f6fb816bead143ae3c915a1f49dce
pseudovicky <sudovicky@gmail.com>
2023-07-27 12:35:21 +0530

    The hurdles race challenge using while loop

--- Day 006/while_loop.py
+++ Day 006/while_loop.py
@@ -20,5 +20,37 @@ while (number_of_hurdles > 0):
     repetitive_block()
     number_of_hurdles -= 1
 
+'''
+while something_is_true:
+    #Do this
+    #Then do this
+    #Then do this
+'''
+
+# Hurdele race
+# The Hurdles Loop Challenge
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def repetitive_block():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+number_of_hurdles = 6 # 4  1
+while (number_of_hurdles > 0):
+    repetitive_block()
+    number_of_hurdles -= 1
+
+    
+    
     
     
