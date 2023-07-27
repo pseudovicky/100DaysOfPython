1ee9efe76e0070b8f07dec5266dc1e6c47f1be01
pseudovicky <sudovicky@gmail.com>
2023-07-27 13:37:32 +0530

    Hurdle juping race track game

---
+++ Day 006/Intractive Coding Challenge/Jumping_over_hurdles.py
@@ -0,0 +1,22 @@
+# The Hurdles Loop Challenge
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
