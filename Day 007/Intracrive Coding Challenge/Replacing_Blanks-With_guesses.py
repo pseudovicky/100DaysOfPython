259b9a14c91ca890d528f22a9241dc0513e46678
pseudovicky <sudovicky@gmail.com>
2023-07-28 11:08:14 +0530

    challenge 2 replace the Blanks

--- Day 007/Intracrive Coding Challenge/Replacing_Blanks-With_guesses.py
+++ Day 007/Intracrive Coding Challenge/Replacing_Blanks-With_guesses.py
@@ -17,16 +17,27 @@ print(f"pssst, the solution is {chosen_word}.")
 
 # ask the user  to guess a letter and assign their answer to a variable called huess. make guess lowercase.
 
-guess = input("Guess a letter: ").lower()
-
 # create an empty list called display.
 # for each lletter in the chosen_word , add a "_" to "display" .
 # so if the chosen_word was "apple", display should be ["_","_","_","_","_"] with 5 "_" representing each letter to guess.
 
+# letter to guess.
+display =[]
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display +=  "_"
+print(display) 
 
-for letter in chosen_word:
+guess = input("Guess a letter: ").lower()
+
+# loop through each position in the chosen_word;
+# if the letter at that position matches 'guess' then reveal that letter in the display at that position.
+
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print('Right')
-    else:
-        print('Wrong')
+        display[position] = letter
 
+print(display)
+
+# print 'display' and you should see the guessed letter in the correct position and every other letter replacing with "_".
