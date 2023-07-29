ab995d1f9b19f9a5fcc52b78fb4dca8c3a6712f9
pseudovicky <sudovicky@gmail.com>
2023-07-29 23:04:57 +0530

    Final project Caesar Cipher

---
+++ Day 008/Day 08 Project/Caesar_Cipher.py
@@ -0,0 +1,33 @@
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+def ceasar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"The {cipher_direction}d text is {end_text}")
+
+from art import logo
+print(logo)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 25
+
+    ceasar(start_text=text,shift_amount=shift,cipher_direction=direction)
+
+    result =  input("Type 'yes' if uou want to go again. otherwise type 'no'.\n")
+    if result == "no":
+        should_continue = False
+        print("GoodBye User!")
