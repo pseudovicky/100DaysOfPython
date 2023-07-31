88abbfb5d30205d13a5cc7c135584df0dcb8b875
pseudovicky <sudovicky@gmail.com>
2023-07-31 14:30:58 +0530

    Calculator project using loops , Flags and Recursion

--- Day 010/Day 10 project/Calculator_program.py
+++ Day 010/Day 10 project/Calculator_program.py
@@ -21,13 +21,29 @@ operations = {
     "/": divide,
 }
 
-num1 = int(input("What's the first number? : "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number?: "))
+def claculator():
+    num1 = int(input("What's the first number? : "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+    while should_continue:
+        operation_symbol = input("Pick an operation : ")
+        num2 = int(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1,num2)
 
-print(f"{num1} {operation_symbol}{num2} = {answer}")
+        print(f"{num1} {operation_symbol}{num2} = {answer}")
+
+        if input("Type 'y' to continue calculating with {answer} or type 'n' to start a new calculation. : ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            claculator()
+
+from art import logo
+print(logo)
+
+claculator()
+
+    
