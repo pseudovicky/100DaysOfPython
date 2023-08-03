d1060c2d20cbf4e6a6d0af0225eb09e6bcca56d1
pseudovicky <sudovicky@gmail.com>
2023-08-03 20:59:20 +0530

    global constant

---
+++ Day 012 Namespaces Local vs. Global Scope/Constant_global_scope.py
@@ -0,0 +1,9 @@
+# There are some variable you never gona be change like
+PI = 3.14159
+URL = "https://www.google.com"
+TWITTER_HANDLE = "@gitviki"
+GITHUB_ID = "@pseudovicky"
+
+# is called Global Constant. 
+# allways write in it Uppercase so that is a reminder for never modify this . 
+
