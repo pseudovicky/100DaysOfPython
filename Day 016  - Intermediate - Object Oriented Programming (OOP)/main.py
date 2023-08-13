2287441dfe5007145a1972eefd417009fdd27354
pseudovicky <sudovicky@gmail.com>
2023-08-13 10:57:55 +0530

    working with turtle

---
+++ Day 016  - Intermediate - Object Oriented Programming (OOP)/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()    # Turtle is a class
+print(timmy)        # timmy is an object of Turtle class. # printed address of object timmy.
+timmy.shape("turtle")
+timmy.color("red")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
