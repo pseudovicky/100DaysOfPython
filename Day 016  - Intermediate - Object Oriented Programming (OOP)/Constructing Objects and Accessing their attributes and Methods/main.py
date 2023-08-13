36135649b5141ddf3e022fd6137e743f23b4a41f
pseudovicky <sudovicky@gmail.com>
2023-08-13 11:05:52 +0530

    Working with Turtle Graphics

--- Day 016  - Intermediate - Object Oriented Programming (OOP)/Constructing Objects and Accessing their attributes and Methods/main.py
+++ Day 016  - Intermediate - Object Oriented Programming (OOP)/Constructing Objects and Accessing their attributes and Methods/main.py
@@ -3,7 +3,8 @@ from turtle import Turtle, Screen
 timmy = Turtle()    # Turtle is a class
 print(timmy)        # timmy is an object of Turtle class. # printed address of object timmy.
 timmy.shape("turtle")
-timmy.color("red")
+timmy.color("coral")
+timmy.forward(100)
 
 my_screen = Screen()
 print(my_screen.canvheight)
