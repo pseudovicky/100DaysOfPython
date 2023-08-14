1a72f5a7ac69c9d19cff2f8566ac970e846942d5
pseudovicky <sudovicky@gmail.com>
2023-08-14 12:12:38 +0530

    question data

--- Day 017 - Intermediate - The Quiz Project & the Benefits of OOP/Quiz Project/data.py
+++ Day 017 - Intermediate - The Quiz Project & the Benefits of OOP/Quiz Project/data.py
@@ -7,11 +7,11 @@ question_data = [
     {"text": "Apporoximately one quater of human bones are in the feet.", "answer": "True"},
     {"text": "The total surface area of human lungs is the size of a football pitch.", "answer": "True"},
     {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, You are entited to atate funeral", "answer": "False"},
     {"text": "It is illegal to get in the Ocean in Portugal.", "answer": "True"},
     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
     {"text": "Google was oroginally called 'Backuub.", "answer": "True"},
     {"text": "Buzz Aldrin's maiden name was 'Moon'.", "answer": "True"},
     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"},
-
 ]
