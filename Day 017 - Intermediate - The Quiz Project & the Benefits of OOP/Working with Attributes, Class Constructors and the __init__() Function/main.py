a60b5b09f88b8fecf8f827b3a3fe1e3e25ebd324
pseudovicky <sudovicky@gmail.com>
2023-08-14 11:03:32 +0530

    attributes

---
+++ Day 017 - Intermediate - The Quiz Project & the Benefits of OOP/Working with Attributes, Class Constructors and the __init__() Function/main.py
@@ -0,0 +1,12 @@
+class User:
+    pass
+
+user_1 = User()
+# create Attributes -  id, username
+# here id and username is simple variable which is associated with an object
+user_1.id = "001"
+user_1.username = "vicky"
+
+print(user_1.username)
+
+
