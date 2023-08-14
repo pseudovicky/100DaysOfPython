1fba6ecce98f386b2c8109e1c6581331025fbbd7
pseudovicky <sudovicky@gmail.com>
2023-08-14 11:59:41 +0530

    question data

---
+++ Day 017 - Intermediate - The Quiz Project & the Benefits of OOP/Quiz Project/questions_class.py
@@ -0,0 +1,11 @@
+#  Quiz project part 1 Creating a Question Class
+# True / Flase
+
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The laudest animal is the africa Elephant.", "answer": "False"},
+    {"text": "Apporoximately one quater of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+    
+]
