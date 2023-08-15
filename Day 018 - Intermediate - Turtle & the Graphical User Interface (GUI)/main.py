02f99550ba7f9695d68a0db3f4cda3ec2e91db8a
pseudovicky <sudovicky@gmail.com>
2023-08-15 20:37:38 +0530

    turtle in python

---
+++ Day 018 - Intermediate - Turtle & the Graphical User Interface (GUI)/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("coral")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
