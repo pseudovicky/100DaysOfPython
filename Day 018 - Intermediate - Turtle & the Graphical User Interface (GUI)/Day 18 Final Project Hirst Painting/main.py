787f028583ae31da4fb1d7ce207e72db8aaaba2f
pseudovicky <sudovicky@gmail.com>
2023-08-16 00:36:54 +0530

    part 1

--- Day 018 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day 18 Final Project Hirst Painting/main.py
+++ Day 018 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day 18 Final Project Hirst Painting/main.py
@@ -1,4 +1,11 @@
 import colorgram
 
+rgb_colors = []
 colors = colorgram.extract('image.jpg', 30)
-print(colors)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
