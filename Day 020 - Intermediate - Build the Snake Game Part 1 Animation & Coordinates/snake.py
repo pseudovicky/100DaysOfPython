46e89388d96a6797e5d62f53e09f3ac29d144daf
pseudovicky <sudovicky@gmail.com>
2023-08-20 00:19:58 +0530

    Snake Game Final Project Day 21 Of #100DaysOfPython

--- Day 020 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/snake.py
+++ Day 020 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/snake.py
@@ -15,18 +15,11 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            
-
-    def add_segment(self, position):
-        new_segment = Turtle("square")
-        new_segment.color("white")
-        new_segment.penup()
-        new_segment.goto(position)
-        self.segments.append(new_segment)
-
-    def extend(self):
-        # add  a new segment to the sanke.
-
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
     
     def move(self):
         for seg_num in range(len(self.segments)-1, 0, -1):
--- Day 021 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/Snake Game Project/food.py
+++ Day 021 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/Snake Game Project/food.py
@@ -5,9 +5,9 @@ class Food(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.shape("square")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.shapesize(stretch_len=1, stretch_wid=1)
         self.color("blue")
         self.speed("fastest")
         self.refresh()
