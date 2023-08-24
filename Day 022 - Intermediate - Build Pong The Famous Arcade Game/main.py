a052e59766f5f7e4c68e55263d997d773bbad1f3
pseudovicky <sudovicky@gmail.com>
2023-08-24 11:13:10 +0530

    turtle project pong game

--- Day 022 - Intermediate - Build Pong The Famous Arcade Game/main.py
+++ Day 022 - Intermediate - Build Pong The Famous Arcade Game/main.py
@@ -11,12 +11,12 @@ r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
 
-
-
 screen.listen()
 screen.onkey(go_up, "Up")
 screen.onkey(go_up, "Down")
 
+game_is_on = True
+while
 
 
 screen.exitonclick()
