7f1ec0431eb34c0d5859dc9ac70d0a97a797144d
pseudovicky <sudovicky@gmail.com>
2023-08-24 18:37:07 +0530

    reset paddle block

--- Day 022 - Intermediate - Build Pong The Famous Arcade Game/main.py
+++ Day 022 - Intermediate - Build Pong The Famous Arcade Game/main.py
@@ -27,6 +27,18 @@ while game_is_on:
     ball.move()
 
     # Detect collision with wall
-    if ball.ycor() > 300 or ball.ycor() <  -300:
-        
+    if ball.ycor() > 280 or ball.ycor() <  -280:
+        ball.bounce_y()
+    
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340 :
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect L paddle
+
+
 screen.exitonclick()
