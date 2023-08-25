939705d4a53e1b53cce14c9f4c19ed101987b296
pseudovicky <sudovicky@gmail.com>
2023-08-25 10:50:43 +0530

    car manager

---
+++ Day 023 - Intermediate - The Turtle Crossing Capstone Project/scoreboard.py
@@ -0,0 +1,13 @@
+from turtle import Turtle 
+
+FONT = ("Courier", 24, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 250)
+        self.write(f"Level: {self.level}",align="left", font=FONT)
