5aeb56c6542530c2a71427d603849f19338f73c6
pseudovicky <sudovicky@gmail.com>
2023-08-27 12:05:56 +0530

    Us states game

--- Day 025 - Intermediate - Working with CSV Data and the Pandas Library/US State Game /main.py
+++ Day 025 - Intermediate - Working with CSV Data and the Pandas Library/US State Game /main.py
@@ -2,10 +2,11 @@ import turtle
 
 screen = turtle.Screen()
 screen.titlr("U.S. States Game")
-image = "blank_states_img.gif"
-
+image = "blank_states_img.jpg"
+screen.addshape(image)
 turtle.shape(image)
 
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+print(answer_state)
 
-screen.exitonclick()
 
