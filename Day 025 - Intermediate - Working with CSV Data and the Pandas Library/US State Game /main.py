bbe39fc2911ab8dbba07719d0d8dbb40580cd1e6
pseudovicky <sudovicky@gmail.com>
2023-08-27 12:22:25 +0530

    US states Game project Saving data in CSV format

--- Day 025 - Intermediate - Working with CSV Data and the Pandas Library/US State Game /main.py
+++ Day 025 - Intermediate - Working with CSV Data and the Pandas Library/US State Game /main.py
@@ -1,12 +1,37 @@
 import turtle
-
+import pandas
 screen = turtle.Screen()
 screen.titlr("U.S. States Game")
 image = "blank_states_img.jpg"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+    print(answer_state)
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break       
+
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+screen.exitonclick()
 
 
