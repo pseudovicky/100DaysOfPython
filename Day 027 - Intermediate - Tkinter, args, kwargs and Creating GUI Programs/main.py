885f014bca104d5e2635d6f780b80f953cdac0e5
pseudovicky <sudovicky@gmail.com>
2023-08-29 04:08:03 +0530

    tkinter

---
+++ Day 027 - Intermediate - Tkinter, args, kwargs and Creating GUI Programs/main.py
@@ -0,0 +1,19 @@
+import tkinter
+
+window  = tkinter.Tk()
+window.title("My first GUI Program")
+window.minsize(width=500, height=300)
+
+
+
+my_label = tkinter.Lable(text="I Am a Lable")
+
+
+
+
+
+
+
+
+
+window.mainloop()
