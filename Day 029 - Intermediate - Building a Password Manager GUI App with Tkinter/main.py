f070c7b34732fb561ed6a953b52ee2551c600d97
pseudovicky <sudovicky@gmail.com>
2023-09-01 12:38:07 +0530

    Dialog Boxex and Pop-Ups in tkinter using messagebox module

--- Day 029 - Intermediate - Building a Password Manager GUI App with Tkinter/main.py
+++ Day 029 - Intermediate - Building a Password Manager GUI App with Tkinter/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from tkinter import messagebox
 
 
 # -------------------------- PASSWORD GENERATOR ----------------------------- #
@@ -12,10 +12,16 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any feilds empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"There are the details entered:\nEmail : {email} \nPassword : {password} \nIs it ok to save?")
+
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # -------------------------- UI SETUP ---------------------------------------- #
