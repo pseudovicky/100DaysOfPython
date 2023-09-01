0853bfcb44d77f211e66ca540bc879007f9c8746
pseudovicky <sudovicky@gmail.com>
2023-09-01 11:03:25 +0530

    challenge 1 screen and  logo setup and

---
+++ Day 029 - Intermediate - Building a Password Manager GUI App with Tkinter/main.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+
+
+# -------------------------- PASSWORD GENERATOR ----------------------------- #
+
+# -------------------------- SAVE PASSWORD ---------------------------------- #
+
+# -------------------------- UI SETUP ---------------------------------------- #
+
+
+
+window = Tk()
+window.title("Password Manager")
+# window.config(padx=28, pady=28)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="/Users/vickys-mackbook-air/100DaysOfPython/Day 029 - Intermediate - Building a Password Manager GUI App with Tkinter/logo.png")
+canvas.create_image(100 , 100, image=logo_img)
+canvas.pack()
+
+
+window.mainloop()
