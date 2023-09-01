282afaca57f929617bec6056d36696c2b100ab59
pseudovicky <sudovicky@gmail.com>
2023-09-01 11:14:27 +0530

    understanding grid columnspan, rowspan

--- Day 029 - Intermediate - Building a Password Manager GUI App with Tkinter/main.py
+++ Day 029 - Intermediate - Building a Password Manager GUI App with Tkinter/main.py
@@ -12,7 +12,7 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-# window.config(padx=28, pady=28)
+window.config(padx=28, pady=28)
 
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="/Users/vickys-mackbook-air/100DaysOfPython/Day 029 - Intermediate - Building a Password Manager GUI App with Tkinter/logo.png")
