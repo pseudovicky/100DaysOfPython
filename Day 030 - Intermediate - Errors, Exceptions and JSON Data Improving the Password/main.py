5690ed41b99a9632fa5432306a5434271727f251
pseudovicky <sudovicky@gmail.com>
2023-09-02 11:57:52 +0530

    FileNotFound Error

--- Day 030 - Intermediate - Errors, Exceptions and JSON Data Improving the Password/main.py
+++ Day 030 - Intermediate - Errors, Exceptions and JSON Data Improving the Password/main.py
@@ -1,4 +1,11 @@
 # Different Type of  Errors :-
 
 
+# 1. FileNotFound Error
+with open("a_file.txt") as file:
+    file.read()
+
+# Error msg = FileNotFoundError: [Errno 2] No such file or directory: 'a_file.txt'
+
+
 
