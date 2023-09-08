c34607b7a727adee16d0d5326cb512cf9b8ee439
pseudovicky <sudovicky@gmail.com>
2023-09-08 11:14:46 +0530

    Capstone Project

--- Day 031 - Intermediate - Flash Card App Capstone Project/main.py
+++ Day 031 - Intermediate - Flash Card App Capstone Project/main.py
@@ -12,14 +12,14 @@ canvas.create_text(400, 150, text='Title', font=('Arial', 40, 'Italic'))
 canvas.create_text(400, 263, text='word', font=('Arial', 68, 'bold'))
 canvas.config(bg=BACKGROUND_COLOR, highlightbackground=0)
 canvas.grid(row=0, column=0)
-
 cross_image = PhotoImage(file='Image/wrong.png')
 unknown_button = Button(image=cross_image)
 unknown_button.grid(row=1, column=0)
-
 check_image = PhotoImage(file='images/right.png')
 known_button =Button(image=check_image, highlightbackground=0)
 known_button.grid(row=1, column=1)
+
+
 
 
 window.mainloop()
